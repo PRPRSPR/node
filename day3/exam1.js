6dd5ec237c8a797df0e629547891c7a8630bbf52
PRPRSPR <s.subin.0208@gmail.com>
2025-04-22 18:14:39 +0900

    250422-RESTful api 사용

---
+++ day3/exam1.js
@@ -0,0 +1,15 @@
+function getFruit(list){
+    let fruitList = [];
+    for(let i=0; i<list.length; i++){
+        if(list[i].length >= 5){
+            fruitList.push(list[i]);
+        }
+    }
+    return fruitList;
+}
+function test(list, callback){
+    return callback(list);
+}
+
+let list = ["apple", "banana", "kiwi", "orange", "blueberry", "pear"];
+let newList = test(list, getFruit);
